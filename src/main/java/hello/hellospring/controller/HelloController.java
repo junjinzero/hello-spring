1caab176029e90e03313898740b2657c3e948bea
junjinzero <progressnull@gmail.com>
2023-11-16 14:49:25 +0900

    7강. MVC와 템플릿 엔진 - Model - View     - 화면에 관한 것만 - Controller     - 비즈니스 로직, 뒷단 로직

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +12,14 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model)
+    {
+        // model 에 담긴 name 이 html 의 ${name} 으로 전달된다
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 
 }

a4ff729f47c6913af89467baa2361a54c4e3e5f0
junjinzero <progressnull@gmail.com>
2023-11-26 14:30:58 +0900

    # 8강. API “객체를 반환하는 것” - `@responseBody` body 부분에 내용을 직접 넣어주겠다 html 화면 소스를 보면 html 태그가 없이 바로 내용이 표시가 된다 - 커맨드 + N  : getter setter  등 바로 만들어 주는 단축키 - 커맨드 + shift + 엔터 : `;` 자동완성 엔터
    xml 방식은 무겁고 상대적으로 번거로움 (열고 닫고 두번 쓰니까)
    최근에는 거의 json 으로 통일이 됐다.
    
    - getter setter : java bean 규약으로, private 으로 지정해두고 게터 세터를 통해 접근을 하게 된다. (프로퍼티 접근 방식이라고도 한다.)
    
    `@ResponseBody` 라는 어노테이션이 붙어있음
    → http 한테 얘를 그대로 넘겨야 되겠구나.
    → 근데 얘가 문자가 아니라 객체야.
    → 객체가 왔네? **객체가 오면 json 방식으로 데이터를 만들어서 http 응답에 반환한다** (기본정책)
    
    - HttpMessageConverter 동작
        - 기본 **문자처리** : StringHttpMessageConverter
        - 기본 **객체처리** : MappingJackson2HttpMessageConverter

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,6 +21,34 @@ public class HelloController {
         // model 에 담긴 name 이 html 의 ${name} 으로 전달된다
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name)
+    {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name)
+    {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 처음으로 문자가 아닌 객체를 넘김
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
